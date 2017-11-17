5e35160084ebbbd1d278eedfaa72be281b53a623
tknr <info@tknr.com>
2017-11-17 09:25:38 +0900

    2017年 11月 17日 金曜日 09:25:38 JST

---
+++ pic.php
@@ -0,0 +1,14 @@
+<?php
+// ///////init//////////
+setlocale(LC_ALL,'ja_JP.UTF-8');
+date_default_timezone_set('Asia/Tokyo');
+require_once __DIR__ . '/lib/ReflexiveLoader.inc';
+$loader = new ReflexiveLoader();
+$loader->registerDir(__DIR__ . '/lib');
+// ///////request////////////////
+$archive = HttpUtil::get("f");
+$page = HttpUtil::getInt("p", 0);
+////////////////
+$book = new Book($archive);
+$book->printPicture($page);
+?>
