b69a6862730ca004035dc6926e695135a7b26942
tknr <info@tknr.com>
2017-11-17 16:21:08 +0900

    2017年 11月 17日 金曜日 16:21:08 JST

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
 $(document).ready(function () {
-	hsize = $(window).height();
-	$("#image").css("height", hsize + "px");
+	imageResize("#image");
 	$("#header").slideUp();
 });
+
 $(window).resize(function () {
-	hsize = $(window).height();
-	$("#image").css("height", hsize + "px");
+	imageResize("#image");
 });
+
 $("#image").click(function(){
 	$("#header").slideToggle();
 });
+
+function imageResize(id){
+	var wsize = $(window).width();
+	var hsize = $(window).height();
+	
+	var imgWidth = $('.sample img').width();
+	var imgHeight = $('.sample img').height();
+	var aspectRatio = imgWidth / imgHeight;
+	
+	if (hsize < imgHeight){
+		$(id).css("height", hsize + "px");
+		return;
+	}
+	
+	if (wsize < imgWidth){
+		$(id).css("width", wsize + "px");
+		return;
+	}
+}
