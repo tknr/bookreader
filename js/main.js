70963197fc6110015a0b8a88810f87947668cf13
tknr <info@tknr.com>
2017-11-17 17:43:18 +0900

    2017年 11月 17日 金曜日 17:43:18 JST

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
 	imageResize("#image");
-	$("#header").slideUp();
+	$("#header").hide("fade");
+	$("#left").hide("fade");
+	$("#right").hide("fade");
 });
 
 $(window).resize(function () {
@@ -8,24 +10,19 @@ $(window).resize(function () {
 });
 
 $("#image").click(function(){
-	$("#header").slideToggle();
+	$("#header").toggle("fade");
+	$("#left").toggle("fade");
+	$("#right").toggle("fade");
 });
 
 function imageResize(id){
-	var wsize = $(window).width();
-	var hsize = $(window).height();
-	
-	var imgWidth = $('.sample img').width();
-	var imgHeight = $('.sample img').height();
-	var aspectRatio = imgWidth / imgHeight;
-	
-	if (hsize < imgHeight){
-		$(id).css("height", hsize + "px");
-		return;
-	}
-	
-	if (wsize < imgWidth){
-		$(id).css("width", wsize + "px");
-		return;
-	}
+	var w = window.innerWidth ? window.innerWidth: $(window).width();
+	var h = window.innerHeight ? window.innerHeight: $(window).height();
+	console.log(w);
+	console.log(h);
+	if (w >= h) {
+        $(id).height(h);
+    } else {
+        $(id).width(w);
+    }
 }
