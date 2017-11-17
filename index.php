56116d2aafa3fe1213fb1bd1de4d08c25220ed01
tknr <info@tknr.com>
2017-11-17 10:28:47 +0900

    2017年 11月 17日 金曜日 10:28:47 JST

--- index.php
+++ index.php
@@ -5,9 +5,42 @@ date_default_timezone_set('Asia/Tokyo');
 require_once __DIR__ . '/lib/ReflexiveLoader.inc';
 $loader = new ReflexiveLoader();
 $loader->registerDir(__DIR__ . '/lib');
+// ///////define//////////
+$define = array();
+{
+    $define['SCRIPT_TITLE'] = 'bookreader'; // Page title
+    $self = array_reverse(explode("/", $_SERVER["SCRIPT_NAME"]));
+    $define['SELF_PHP'] = $self[0];
+    $define['SCRIPT_PATH'] = rtrim($_SERVER["SCRIPT_NAME"], $define['SELF_PHP']);
+    $define['CHARSET'] = 'UTF-8'; // Shift_JIS
+    $define['APC_TTL'] = 60 * 60 * 0.5;
+	$define['TEMPLATE_FOLDER'] = __DIR__ . '/template/';
+}
+// config
+{
+	$define['HOME_DIR'] = '/home/tknr/';
+}
+APCUtil::define_array($define['SCRIPT_TITLE'], $define,false);
 // ///////request////////////////
-$archive = HttpUtil::get("f");
+$filename = HttpUtil::get("f");
+$archive = HOME_DIR.$filename;
 $page = HttpUtil::getInt("p", 0);
-?>
-////////////////
+////////// book
 $book = new Book($archive);
+
+// render
+$template = new EZTemplate(TEMPLATE_FOLDER . 'index.inc');
+$template->setValue('filename', $filename);
+$template->setValue('page', $page);
+$template->setValue('minpage', $book->getMinPage());
+$template->setValue('maxpage', $book->getMaxPage());
+
+$template->setvalue('image_url','pic.php?p='.$page.'&f='.$filename);
+$template->setvalue('alt',$book->getFilename($page));
+
+
+$template->setReplace('%CHARSET%', CHARSET);
+$template->setReplace('%TITLE%', SCRIPT_TITLE.':'.$book->getArchiveFilename());
+$template->setReplace('%SELF%', SELF_PHP);
+return $template->render();
+?>
--- pic.php
+++ pic.php
@@ -5,8 +5,24 @@ date_default_timezone_set('Asia/Tokyo');
 require_once __DIR__ . '/lib/ReflexiveLoader.inc';
 $loader = new ReflexiveLoader();
 $loader->registerDir(__DIR__ . '/lib');
+// ///////define//////////
+$define = array();
+{
+    $define['SCRIPT_TITLE'] = 'bookreader_pic'; // Page title
+    $self = array_reverse(explode("/", $_SERVER["SCRIPT_NAME"]));
+    $define['SELF_PHP'] = $self[0];
+    $define['SCRIPT_PATH'] = rtrim($_SERVER["SCRIPT_NAME"], $define['SELF_PHP']);
+    $define['CHARSET'] = 'UTF-8'; // Shift_JIS
+    $define['APC_TTL'] = 60 * 60 * 0.5;
+}
+// config
+{
+	$define['HOME_DIR'] = '/home/tknr/';
+}
+APCUtil::define_array($define['SCRIPT_TITLE'], $define,false);
 // ///////request////////////////
-$archive = HttpUtil::get("f");
+$filename = HttpUtil::get("f");
+$archive = HOME_DIR.$filename;
 $page = HttpUtil::getInt("p", 0);
 ////////////////
 $book = new Book($archive);
