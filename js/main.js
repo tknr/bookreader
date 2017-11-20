3d9a8d432c589b753600509bf077b90972a0241b
tknr <info@tknr.com>
2017-11-20 09:52:54 +0900

    2017年 11月 20日 月曜日 09:52:54 JST

--- js/main.js
+++ js/main.js
@@ -4,15 +4,13 @@ $(document).on("mobileinit", function(){
 	
 	$("#header").hide("fade");
 	$("#footer").hide("fade");
+	
+	imageResize("#image");
 });
 
 $("#content").click(function(){
 	$("#header").toggle("fade");
 	$("#footer").toggle("fade");
-});
-
-$(window).on('load',function() {
-	imageResize("#image");
 });
 
 $(window).resize(function () {
@@ -52,29 +50,4 @@ function imageResize(selector){
 	
 	$(selector).width(width);
     $(selector).height(height);
-}
-
-function getWindowHeight(){
-	// 端末の向きを算出
-    var isPortrait = window.innerHeight > window.innerWidth;
-    // UserAgent から端末の種類を判別
-    var ua = navigator.userAgent;
-    var device;
-    if (ua.search(/iPhone/) != -1 || ua.search(/iPod/) != -1) {
-        device = "iPhone";
-    } else if (ua.search(/Android/) != -1) {
-        device = "Android";
-    }
-    
-    var h;
-    // 端末の種類からページの高さを算出
-    if (device == "Android") {
-        h = Math.round(window.outerHeight / window.devicePixelRatio);
-    } else if (device == "iPhone") {
-        bar = (isPortrait ? 480 : screen.width) - window.innerHeight - (20 + (isPortrait ? 44 : 32));
-        h = window.innerHeight + bar;
-    } else {
-        h = window.innerHeight;
-    }
-    return h;
 }
